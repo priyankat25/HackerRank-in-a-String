c06bf6829acce5aead5c6944d17858e8d543760f
priyankat25 <161025060+priyankat25@users.noreply.github.com>
2024-04-30 14:04:46 +0530

    Add files via upload

---
+++ HackerRankInAString/src/com/kodnest/hrs/HackerRankInAString.java
@@ -0,0 +1,61 @@
+package com.kodnest.hrs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+class Result {
+
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String hackerrankInString(String s) {
+    // Write your code here
+    
+        String str = "hackerrank";
+        // String ar="";
+        int i = 0;
+        for(int j=0; j<s.length(); j++){
+            if(str.charAt(i) == s.charAt(j)){
+                i++;
+            }  
+            if(str.length() == i){
+                return "YES";
+            }
+        }
+         return "NO";
+    }
+
+}
+
+public class HackerRankInAString {
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.hackerrankInString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
